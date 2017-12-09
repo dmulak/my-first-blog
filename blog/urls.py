e4e4fdbcc90b38d72d671e91d32e7ad4b8156f94
Dominika <dominika.mulak@gmail.com>
2017-12-09 16:19:56 +0000

    fixed post detais page

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^post/(?P<pid>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'post/^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^$', views.post_list, name='post_list'),
 ]
